927969b968c00a2d07c6ee3d0cbbd1ac48c02f45
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2023-02-06 23:55:25 -0600

    Documentación in-line
    Documentación interna de la clase para redes bayesianas. Se comentan todos los métodos, dando una guía de la funcionalidad de cada uno. Aporte grupo #1.

--- src/bayesiannetworkai/bayesiannetworkai.py
+++ src/bayesiannetworkai/bayesiannetworkai.py
@@ -3,16 +3,34 @@ from pgmpy.factors.discrete.CPD import TabularCPD
 import numpy as np
 from pgmpy.inference import VariableElimination
 
+
+
+
+# Clase que representa una red bayesiana y sus operaciones
+# recibe como parametros una lista de tuplas que representan las relaciones entre nodos
+# y una lista de tuplas que representan los nodos y sus atributos
 class BayesianNetwork(object):
 
+    
+    # Constructor de la clase
+    # recibe como parametros una lista de tuplas que representan las relaciones entre nodos
+    # y una lista de tuplas que representan los nodos y sus atributos
     def __init__(self, nodos, nodos_atributes):
+
+        # Se inicializa el modelo de red bayesiana utilizando la lista de relaciones entre nodos
         self.model = BN(nodos)
         self.nodos_atributes = nodos_atributes
+
+        # Se construye el grafo de la red bayesiana
         self.build_graph(nodos)
         self.cps = {}
+
+        # Se calculan las probabilidades de los nodos
         self.set_probabilities()
         # self.factors()
         
+    
+    # Construir el grafo de la red bayesiana
     def build_graph(self, nodes):
         self.graph = {}
         for node in nodes:
@@ -22,13 +40,16 @@ class BayesianNetwork(object):
             else:
                 self.graph[node[0]] = [node[1]]
 
+    
+    # Metodo que calcula la probabilidad de que un nodo sea cierto dado un conjunto de evidencias
     def set_probabilities(self):
+        # Lista de tablas de probabilidad condicional de los nodos
         cpds = []
+
         for nodo in self.nodos_atributes:
             if len(nodo) == 2:
                 element = TabularCPD(nodo[0], 2, nodo[1])
-                self.cps[nodo[0]] = [nodo[1]]
-                
+                self.cps[nodo[0]] = [nodo[1]]        
             else:
                 temp = []
                 for i in range(len(nodo[2])):
@@ -37,22 +58,34 @@ class BayesianNetwork(object):
                 self.cps[nodo[0]] = [nodo[1], nodo[2]]
             cpds.append(element)
         
+        # Se agregan las tablas de probabilidad condicional al modelo de red bayesiana
         for element in cpds:
             self.model.add_cpds(element)
 
+    
+    # Método de enumeración para la red bayesiana para consultar la probabilidad de un nodo dado un conjunto de evidencias
     def enumeration(self,node,evidence):
         inference = VariableElimination(self.model)
         return inference.query(variables =[node], evidence=evidence)
     
         
+    # Método para imprimir los factores de la red bayesiana
     def factors(self):
         factores = self.model.get_cpds()
         for factor in factores:
             print(factor)
     
+    
+    # Método para saber si la red bayesiana esta completamente descrita o no
+    # retorna True si la red bayesiana esta completamente descrita
     def fully_described(self):
         print("Completamente descrita" if self.model.check_model() else "No completamente descrita")
 
+    
+    
+    # Método para obtener la representación de la red bayesiana en forma compacta
+    # retorna una cadena de texto con la representación de la red bayesiana en forma compacta
+    # Ejemplo: P(A,B,C) = P(A|B,C)P(B|C)P(C)
     def compactness_representation(self):
         parent_relation = {}
         for node in self.graph:
